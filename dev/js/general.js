1c6e67d66eabd93967b7cd361e3567aa480ed562
Vira Lytvyn <baterfluy@gmail.com>
2014-06-28 15:45:54 +0300

    Added function to provide animation

--- dev/js/general.js
+++ dev/js/general.js
@@ -5,3 +5,18 @@ function attachReaction (event, element, func) {
         return element.addEventListener(event, func, false);
     }
 }
+function animate(object, property, start_value, end_value, time) {
+    var frame_rate = 0.06; // 60 frame per second
+    var frame = 0;
+    var delta = (end_value - start_value) / time / frame_rate;
+    var handle = setInterval(function() {
+        frame++;
+        var value = start_value + delta * frame;
+        object.style[property] = value + "px";
+        if (value == end_value) {
+            clearInterval(handle);
+        }
+    }, 1 / frame_rate);
+}
+
+//animate(document.getElementById("a"), "top", 0, 100, 1000);
