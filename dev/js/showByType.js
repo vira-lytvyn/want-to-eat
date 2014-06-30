609d0875a0870864f350ac378ac8ae162e3dbfcc
BuljaBulja <boikovych.photo@gmail.com>
2014-06-30 18:24:14 +0300

    changes for new json

--- dev/js/showByType.js
+++ dev/js/showByType.js
@@ -1,6 +1,7 @@
 /**
  * Created by Administrator on 29.06.2014.
  */
+console.log('hello');
 function showByType(type, arr) {
     var typeArr = [];
     for (var i = 0; i < arr.length; i++){
@@ -8,8 +9,8 @@ function showByType(type, arr) {
             typeArr.push(arr[i]);
         }
     }
-
+    console.log(typeArr);
     return typeArr;
 }
 
-showByType(false, data);
+//showByType(false, data);
