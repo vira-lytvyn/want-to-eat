91216a6d790256017798b3d4660bd3fdad7cc251
BuljaBulja <boikovych.photo@gmail.com>
2014-06-30 15:24:46 +0300

    improved final recipe page

--- dev/js/recipes.js
+++ dev/js/recipes.js
@@ -4,77 +4,117 @@ function buildRecipeDetail(id) {
     var recipe = document.createElement('div');
     recipe.id = 'doneRecipe';
 
-    var image = document.createElement('img');
-    image.src = currentRecipe.picture;
-    recipe.appendChild(image);
-
-    var header = document.createElement('h1');
-    header.innerHTML = currentRecipe.title;
+    //->start of header block
+    var header = document.createElement('div');
+    header.className = 'main-blocks';
     recipe.appendChild(header);
 
-    var author = document.createElement('author');
-    author.innerHTML = 'author: ' + currentRecipe.author + '; ';
+    var image = document.createElement('img');
+    image.src = currentRecipe.picture;
+    header.appendChild(image);
+
+    var title = document.createElement('h1');
+    title.innerHTML = currentRecipe.title;
+    header.appendChild(title);
+    //X end of header block
+
+    //->start of info block
+    var author = document.createElement('a');
+    author.className = 'main-blocks info';
+    author.id = 'author';
+    author.innerHTML = 'author: ' + currentRecipe.author;
+    author.onclick = function() {
+          alert('search by author ' + currentRecipe.author);
+    };
     recipe.appendChild(author);
 
-    var time = document.createElement('time');
-    time.innerHTML = 'added: ' + currentRecipe.added + ';';
-    recipe.appendChild(time);
+    var rank = document.createElement('div');
+    rank.className = 'main-blocks info';
+    rank.innerHTML = 'social rank: ' + currentRecipe.social_rank;
+    recipe.appendChild(rank);
+    //X end of info block
 
-    var ingredients = document.createElement('ol');
+    //->start of ingredients block
+    var ingredients = document.createElement('div');
+    ingredients.className = 'main-blocks';
+    ingredients.id = 'ingredients';
+
+    var ingTitle = document.createElement('div');
+    ingTitle.innerHTML = 'ingredients you need to...';
+    ingTitle.id = 'ingTitle';
+    ingTitle.onclick = function() {
+        if (ingList.style.display == 'none') {
+            ingList.style.display = 'block';
+        } else {
+            ingList.style.display = 'none';
+        }
+    };
+    ingredients.appendChild(ingTitle);
+
+    var ingList = document.createElement('ol');
+    ingList.id = 'ingList';
+    ingList.className = 'sub-blocks';
+    ingList.style.display = 'none';
     var ingData = currentRecipe.ingredients;
     for (var i = 0; i < ingData.length; i++) {
-        ingredients.innerHTML += '<li>' + ingData[i] + '</li>';
+        ingList.innerHTML += '<li>' + ingData[i] + '</li>';
     }
-    var ingSpan = document.createElement('span');
-    ingSpan.innerHTML = 'you need such ingredients:<br/>';
-    ingredients.insertBefore(ingSpan, ingredients.firstChild);
-    recipe.appendChild(ingredients);
+    ingredients.appendChild(ingList);
 
-    var description = document.createElement('p');
-    description.innerHTML = currentRecipe.description;
+    recipe.appendChild(ingredients);
+    //->X end of ingredients block
+
+    //->start of description block
+    var description = document.createElement('div');
+    description.className = 'main-blocks';
+
+    var descText = document.createElement('p');
+    descText.innerHTML = currentRecipe.description;
+    description.appendChild(descText);
+
+    var mealCat = document.createElement('span');
+    mealCat.id = 'mealCat';
+    mealCat.innerHTML = currentRecipe.category;
+    description.appendChild(mealCat);
+
+    var mealType = document.createElement('span');
+    mealType.id = 'mealType';
+    mealType.innerHTML = currentRecipe.type_of_meal;
+    description.appendChild(mealType);
+
+    var time = document.createElement('p');
+    time.innerHTML = 'added: ' + currentRecipe.added;
+    description.appendChild(time);
+
     recipe.appendChild(description);
+    //->X end of description block
+
+    //->start of links block
+    var source = document.createElement('a');
+    source.className = 'main-blocks';
+    source.href = currentRecipe.source_url;
+    source.id = 'source';
+    source.innerHTML = 'Source';
+    recipe.appendChild(source);
 
     var save = document.createElement('input');
     save.value = 'save';
     save.type = 'button';
+    save.className = 'main-blocks button';
     recipe.appendChild(save);
     attachReaction('click', save, createCookie('recipe_' + id, id, 7));
 
     var share = document.createElement('input');
     share.value = 'share by e-mail';
     share.type = 'button';
+    share.className = 'main-blocks button';
     share.onclick = function (){
-        sendEmail(currentRecipe);
+        alert('send E-mail');
     };
     recipe.appendChild(share);
+    //->X end of links block
 
     document.getElementById('placeHolder').appendChild(recipe);
 }
 
 buildRecipeDetail(5);
-
-function sendEmail(dataRecipe) {
-
-    console.log(dataRecipe);
-
-    var box = document.createElement('fomr');
-    box.id = 'sendEmail';
-    box.style.display = 'block';
-    box.setAttribute ('action', '#');
-
-    var email = document.createElement('input');
-    email.type = 'email';
-    box.appendChild(email);
-
-
-    var send = document.createElement('input');
-    send.type = 'submit';
-    send.value = 'send';
-    send.onclick = function() {
-        box.style.display = 'none';
-    };
-    box.appendChild(send);
-
-    document.getElementById('placeHolder').appendChild(box);
-    box.style.marginLeft = parseInt(getComputedStyle(box).marginLeft) - (box.clientWidth / 2) + 'px';
-}
