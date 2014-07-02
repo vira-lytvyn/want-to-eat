233c25efe827ae8f89274d806884c76708515d38
Vira Lytvyn <baterfluy@gmail.com>
2014-07-02 19:01:45 +0300

    Moved universal sorting function from separate script file to general script file

--- dev/js/general.js
+++ dev/js/general.js
@@ -19,4 +19,19 @@ function animate(object, property, start_value, end_value, time) {
     }, 1 / frame_rate);
 }
 
-//animate(document.getElementById("a"), "top", 0, 100, 1000);
+function sortBy(target, data) {
+    if (typeof data[0][target] === 'number') {
+        data.sort(function (a, b) {
+            return b[target] - a[target];
+        });
+    } else if (typeof data[0][target] === 'string') {
+        data.sort(function (a, b) {
+            if (a[target] < b[target]) return -1;
+            if (a[target] > b[target]) return 1;
+            return 0;
+        });
+    } else {
+        console.log('Error: bad type of target!');
+    }
+    console.log(data);
+}
