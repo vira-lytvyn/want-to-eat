a0a371d4f6f18addcbd80855d157a820f055b274
BuljaBulja <boikovych.photo@gmail.com>
2014-07-04 18:27:21 +0300

    added animate

--- dev/js/general.js
+++ dev/js/general.js
@@ -55,3 +55,17 @@ function clearElementContent(element) {
         element.removeChild(element.firstChild);
     }
 }
+
+function animate(object, property, start_value, end_value, time) {
+    var frame_rate = 0.06; // 60 frame per second
+    var frame = 0;
+    var delta = (end_value - start_value) / time / frame_rate;
+    var handle = setInterval(function() {
+        frame++;
+        var value = start_value + delta * frame;
+        object.style[property] = value + "px";
+        if (value == end_value) {
+            clearInterval(handle);
+        }
+    }, 1 / frame_rate);
+}
