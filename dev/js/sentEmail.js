c16472b8f423cdd4d318829d280e8f104146fbb5
Vira Lytvyn <baterfluy@gmail.com>
2014-07-07 01:31:49 +0300

    Implemented sending by email data binding

--- dev/js/sentEmail.js
+++ dev/js/sentEmail.js
@@ -53,57 +53,58 @@ EmailRequestConstructor.prototype.sendRequest = function (params, onresult, oner
     return request.send(params);
 };
 
-var myRequest = new EmailRequestConstructor();
-console.log('test0');
-var testEmailObj = {
-    message: {
-        'from_email': 'tangerines.epam@gmail.com',
-        'to': [
-            {
-                'email': 'baterfluy@gmail.com',
-                'name': 'Vira',
-                'type': 'to'
-            },
-            {
-                'email': 'RECIPIENT_NO_2@EMAIL.HERE',
-                'name': 'ANOTHER RECIPIENT NAME (OPTIONAL)',
-                'type': 'to'
-            }
-        ],
-        'autotext': 'true',
-        'subject': 'Example_Subject',
-        'html': generateEmailContent()
-    }
-};
-
-//myRequest.sendRequest(testEmailObj);
-
-function fillEmailObj(recipeObj) {
-
-}
+var emailRequest = new EmailRequestConstructor();
 
 function sendRecipeMail() {
     event.preventDefault();
-    var object = JSON.parse(document.querySelector('#chosenRecipe').value);
-    console.log(object);
     var form = event.target || event.srcElement;
-    console.log(form.articleName.value);
+    var object = JSON.parse(document.querySelector('#chosenRecipe').value);
+    generateEmailContent(object, form.comment.value);
+    console.log();
+    var recipients = form.recipientsEmails.value.split(',');
+
+    if (!form.senderEmail.value || !recipients.length) {
+        emailRequest.onError();
+        return;
+    }
+
+    var toArray = [];
+    for (var i = 0; i < recipients.length; i++){
+        toArray.push({
+            'email': recipients[i],
+            'type': 'to'
+        });
+    }
+
+    var emailObj = {
+        message: {
+            'from_email': form.senderEmail.value,
+            'to': toArray,
+            'autotext': 'true',
+            'subject': form.articleName.value,
+            'html': generateEmailContent(object, form.comment.value)
+        }
+    };
+    emailRequest.sendRequest(emailObj);
 }
 
-function generateEmailContent() {
+function generateEmailContent(obj, message) {
+    message = message || 'Just look at this awesome recipe =)';
     var html = '' +
         '<div style="background: #DCECFD;width: 600px;margin: 0 auto;padding: 20px;-webkit-border-radius: 15px;-moz-border-radius: 15px;border-radius: 15px;border: 1px solid;">' +
         '        <img src="http://i1268.photobucket.com/albums/jj564/Vira_Lytvyn/logo_zps9f915a7c.png" style="height: 100px;display: block;margin: 0 auto;">' +
         '        <hr style="height: 2px;background-color: #ccc;border: none;margin: 20px 0;">' +
         '        <div style="overflow: hidden;">' +
-        '            <img src="http://pad1.whstatic.com/images/thumb/f/f0/Make-Starbucks-Lemon-Loaf-Step-9.jpg/670px-Make-Starbucks-Lemon-Loaf-Step-9.jpg" style="max-width: 100px;max-height: 100px;float: left;margin-right: 20px;" width="100" height="100">' +
-        '            <a href="http://www.wikihow.com/Make-Starbucks-Lemon-Loaf">' +
-        '                <h2 style="white-space: nowrap;text-overflow: ellipsis;overflow: hidden;">Starbucks Lemon Loaf</h2>' +
+        '            <img src="' + obj.picture + '" style="max-width: 100px;max-height: 100px;float: left;margin-right: 20px;" width="100" height="100">' +
+        '            <a href="' + obj.source_url + '">' +
+        '                <h2 style="white-space: nowrap;text-overflow: ellipsis;overflow: hidden;">' + obj.title + '</h2>' +
         '            </a>' +
-        '            <p>You might have a special place in your heart for the delicious pastries at Starbucks, but you don\'t have to go there to enjoy this one.</p>' +
+        '            <p>' + obj.description + '</p>' +
         '        </div>' +
         '        <hr style="height: 2px;background-color: #ccc;border: none;margin: 20px 0;">' +
-        '        <div style="font-style: italic;">This is my suggestions about our supper )) </div>' +
+        '        <div style="font-style: italic;">' + message + '</div> ' +
+        '        <hr style="height: 2px;background-color: #ccc;border: none;margin: 20px 0;">' +
+        '        <div style="text-align: center">&copy; Want to Eat, 2014</div>' +
         '</div>';
     return html;
 }
