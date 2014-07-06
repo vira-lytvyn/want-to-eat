6b718d62af3c7be511eabe9a303e0da482d73bfe
Vira Lytvyn <baterfluy@gmail.com>
2014-07-06 19:36:32 +0300

    Fixed "Uncaught TypeError: object is not a function" associated with send recipe form

--- dev/js/sentEmail.js
+++ dev/js/sentEmail.js
@@ -1,57 +1,57 @@
-/**
- * Created by CHICHEK on 05.07.2014.
- */
-
-function EmailRequestConstructor () {
-    this.rootUrl = 'https://mandrillapp.com/api/1.0/messages/send.json';
-    this.key  = 'h4MHieHtpFv7UTWP5oQs6w' ;
-}
-
-EmailRequestConstructor.prototype.sendRequest = function (params, onresult, onerror) {
-    console.log('test2');
-    var request,
-        _this = this;
-    if (params == null) {
-        params = {};
-    }
-    params.key = this.key;
-    params = JSON.stringify(params);
-    request = new XMLHttpRequest();
-    request.open('POST', this.rootUrl);
-    request.setRequestHeader('Content-Type', 'application/json');
-    request.onreadystatechange = function() {
-        console.log('test3');
-        var response;
-        if (request.readyState !== 4) {
-            return;
-        }
-        response = JSON.parse(request.responseText);
-        if (response == null) {
-            console.log('test4');
-            response = {
-                status: 'error',
-                name: 'GeneralError',
-                message: 'An unexpected error occurred'
-            };
-        }
-        if (request.status !== 200) {
-            if (onerror) {
-                console.log('error', response);
-                return onerror(response);
-            } else {
-                console.log('error', response);
-                return _this.onerror(response);
-            }
-        } else {
-            console.log('result', response);
-            if (onresult) {
-                console.log('result', response);
-                return onresult(response);
-            }
-        }
-    };
-    return request.send(params);
-};
+///**
+// * Created by CHICHEK on 05.07.2014.
+// */
+//
+//function EmailRequestConstructor () {
+//    this.rootUrl = 'https://mandrillapp.com/api/1.0/messages/send.json';
+//    this.key  = 'h4MHieHtpFv7UTWP5oQs6w' ;
+//}
+//
+//EmailRequestConstructor.prototype.sendRequest = function (params, onresult, onerror) {
+//    console.log('test2');
+//    var request,
+//        _this = this;
+//    if (params == null) {
+//        params = {};
+//    }
+//    params.key = this.key;
+//    params = JSON.stringify(params);
+//    request = new XMLHttpRequest();
+//    request.open('POST', this.rootUrl);
+//    request.setRequestHeader('Content-Type', 'application/json');
+//    request.onreadystatechange = function() {
+//        console.log('test3');
+//        var response;
+//        if (request.readyState !== 4) {
+//            return;
+//        }
+//        response = JSON.parse(request.responseText);
+//        if (response == null) {
+//            console.log('test4');
+//            response = {
+//                status: 'error',
+//                name: 'GeneralError',
+//                message: 'An unexpected error occurred'
+//            };
+//        }
+//        if (request.status !== 200) {
+//            if (onerror) {
+//                console.log('error', response);
+//                return onerror(response);
+//            } else {
+//                console.log('error', response);
+//                return _this.onerror(response);
+//            }
+//        } else {
+//            console.log('result', response);
+//            if (onresult) {
+//                console.log('result', response);
+//                return onresult(response);
+//            }
+//        }
+//    };
+//    return request.send(params);
+//};
 
 //var myRequest = new EmailRequestConstructor();
 console.log('test0');
@@ -82,6 +82,7 @@ function fillEmailObj(recipeObj) {
 
 }
 
-function sendRecipe() {
-    console.log('test');
+function sendRecipeMail() {
+    var field = event.target || event.srcElement;
+    console.log(field);
 }
