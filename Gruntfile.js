68fb1f3a256a6b3473c318ed23a040b6b3fce15e
Vira Lytvyn <baterfluy@gmail.com>
2014-07-14 22:56:43 +0300

    Added validation tasks to verify and standard html, sass, css ans js code

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,21 +39,77 @@ module.exports = function(grunt) {
                 }
             }
         },
+        jshint: {
+            gruntfile: ['Gruntfile.js'],
+            custom: {
+                options: {
+                    globals: {
+                        jQuery: false,
+                        console: false,
+                        module: false,
+                        document: true
+                    }
+                },
+                files: {
+                    src:['dev/js/*.js']
+                }
+            }
+        },
+        validation: {
+            options: {
+                reset: grunt.option('reset') || false,
+                stoponerror: false,
+                relaxerror: [
+                    'Section lacks heading. Consider using h2-h6 elements to add identifying headings to all sections.'
+                ],
+                path: '/grunt-modules-config/validation-status.json',
+                reportpath: '/grunt-modules-config/validation-report.json'
+            },
+            files: {
+                src: ['dev/index.html']
+            }
+        },
+        csslint: {
+            main: {
+                src: ['build/css/*.css']
+            }
+        },
+        scsslint: {
+            modules: [
+                'dev/scss/*.scss'
+            ],
+            options: {
+                reporterOutput: 'grunt-modules-config/scss-lint-report.xml',
+                colorizeOutput: true,
+                config: 'grunt-modules-config/scss-lint-config.yml'
+            }
+        },
+        csscomb: {
+            options: {
+                config: 'grunt-modules-config/csscomb.json'
+            },
+            dynamic_mappings: {
+                expand: true,
+                cwd: 'dev/scss/',
+                src: ['*.scss', '!style.scss'],
+                dest: 'dev/scss/'
+            }
+        },
         watch: {
             options: {
                 livereload: true
             },
             html: {
                 files: ['dev/*.html'],
-                tasks: ['newer:copy:main']
+                tasks: ['validation', 'newer:copy:main']
             },
             scripts: {
                 files: ['dev/js/*.js'],
-                tasks: ['newer:concat', 'uglify']
+                tasks: ['newer:jshint:custom', 'newer:concat', 'uglify']
             },
             style: {
                 files: ['dev/scss/*.scss'],
-                tasks: ['sass'],
+                tasks: ['csscomb', 'scsslint', 'newer:sass', 'csslint'],
                 options: {
                     spawn: false
                 }
@@ -69,8 +125,14 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-csscomb');
+    grunt.loadNpmTasks('grunt-scss-lint');
+    grunt.loadNpmTasks('grunt-contrib-csslint');
+    grunt.loadNpmTasks('grunt-html-validation');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-newer');
 
-    grunt.registerTask('default', ['copy', 'concat', 'uglify', 'sass']);
+    grunt.registerTask('default', ['validate', 'copy', 'concat', 'uglify', 'sass']);
+    grunt.registerTask('validate', ['jshint', 'validation', 'csscomb', 'scsslint', 'csslint']);
 };
